d117dfc2f67b73950e488c798f8070dde997d8fa
lat2509 <tuangioig@gmail.com>
2025-08-18 14:59:03 +0700

    udapte

--- FetchAPI/bài1.js
+++ FetchAPI/bài1.js
@@ -2,4 +2,3 @@ fetch('https://fakestoreapi.com/products')
   .then(response => response.json())
   .then(data => data.map(product =>  product.title))
   .then(item => console.log(item))
-  .catch(error => console.error('Error:', error));
